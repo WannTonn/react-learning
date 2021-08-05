217a0f57f04ad70458771a31392aaf2a17dc1e24
WannTonn <895522268@qq.com>
2021-08-05 22:59:32 +0800

    feat: 更新todoList

--- src/views/ToDoList/index.js
+++ src/views/ToDoList/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+
 import './style.scss';
 
 const TodoList = () => {
+
+  const [inputVal, setInputVal] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const firstRender = useRef(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputVal) { return false };
+    setTodoList([...todoList, {
+      id: new Date().getTime(),
+      name: inputVal
+    }])
+
+    setInputVal('');
+  }
+  const handleDelThing = (e) => {
+    setTodoList(todoList.filter((todo) => todo.id !== e));
+  }
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+    } else {
+      localStorage.setItem('TodoItem', JSON.stringify([...todoList]))
+    }
+  }, [todoList])
+  useEffect(() => {
+    const newTodoList = localStorage.getItem('TodoItem');
+    setTodoList(JSON.parse(newTodoList ?? []));
+  }, [])
   return (
-    <>
-      TodoList
-    </>
+    <div className='App'>
+      <div className="todoListWrapper">
+        <form onSubmit={handleSubmit}>
+          <div className='inputWrapper'>
+
+            <input className='todoInput' type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)} placeholder='请输入今天要做的事情' />
+            <button className='addBtn' type='submit'>Add</button>
+          </div>
+        </form>
+        <div className="todoLists">
+          {
+            todoList.map((e, index) => (
+              <div className='item' key={e.id}>
+                <div className='thing'>
+                  {e.name}
+                </div>
+                <div className='delBtn' onClick={() => handleDelThing(e.id)}>x</div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/views/components/button/index.js
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import './style.scss';
+
+const Button = ({props}) => {
+
+  return (
+    <div className='Button'>{}</div>
+  )
+}
+props = {
+  
+}
+export default Button;
