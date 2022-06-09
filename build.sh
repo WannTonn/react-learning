42d851e6b9395b595bf01b8f9bfa6c76d579c3ca
WannTonn <895522268@qq.com>
2022-06-09 18:17:44 +0800

    feat:update config files

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ echo "delete docs folder"
 rm -rf ./docs
 echo "rename folder name from build to docs "
 mv ./build ./docs
+echo "add changed files"
+git add .
+echo "commit changed files"
+git commit -m "feat: update files"
+echo "push changed files"
+git push origin master
