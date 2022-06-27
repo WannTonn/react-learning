98a2e20406097f383a27e1a19826e44b39d61bd5
WannTonn <895522268@qq.com>
2022-06-27 23:02:57 +0800

    feat:update files

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import 'antd/dist/antd.min.css';
 import './global.scss';
 // import TodoList from './views/ToDoList'
